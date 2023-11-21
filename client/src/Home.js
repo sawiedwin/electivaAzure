71a25045c468edcc8fcf97ce782a00a0901fb38a
sawiedwin <edwin.quinterogu@campusucc.edu.co>
2023-11-20 20:43:35 -0500

    Electiva Azure

---
+++ client/src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Esto es una prueba de un app service en azure</h2>
+                <h3>Edwin Quintero Guzman-Ingeniero de Software-Quinto Semestre</h3>
+            </div>
+        );
+    }
+}
+
+export default Home;
